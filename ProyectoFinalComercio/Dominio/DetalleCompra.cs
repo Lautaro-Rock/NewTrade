2c6c7eb276e6540d77135abc084432c984a01852
Lautaro Maldonado <lautylty@hotmail.com>
2025-06-01 18:17:34 -0300

    class faltantes

---
+++ ProyectoFinalComercio/Dominio/DetalleCompra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class DetalleCompra
+    {
+        public int IdDetalleCompra { get; set; }
+        public int IdCompra { get; set; }
+        public Compra Compra { get; set; }
+        public int IdProducto { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
---
+++ ProyectoFinalComercio/Dominio/ProductoProveedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class ProductoProveedor
+    {
+        public int IdProducto { get; set; }
+        public Producto Producto { get; set; }
+        public int IdProveedor { get; set; }
+        public Proveedor Proveedor { get; set; }
+    }
+}
